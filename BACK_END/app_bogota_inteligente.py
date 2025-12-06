36db9c3a238c0714a7cdb7237f3a24b2f1bc945f
andres-fuentex <andresfuentesgs@gmail.com>
2025-12-06 00:13:48 -0500

    text2

--- BACK_END/app_bogota_inteligente.py
+++ BACK_END/app_bogota_inteligente.py
@@ -91,11 +91,11 @@ with col_info4:
 
 with col_info5:
     st.markdown("### 🩺 Salud")
-    st.info("Prioriza el cuidado. Ubica la red de **Hospitales y Clínicas (IPS)** disponibles para atender urgencias médicas.")
+    st.info("Prioriza el cuidado. Ubica la red de **Hospitales y Clínicas (IPS)** disponibles.")
 
 with col_info6:
     st.markdown("### 🌳 Zonas Verdes")
-    st.success("Respira mejor. Encuentra los **Parques y Pulmones Verdes** ideales para el deporte, el descanso o pasear a tu mascota.")
+    st.success("Respira mejor. Encuentra las **Zonas Verdes** ideales para el deporte, el descanso o pasear a tu mascota.")
 
 st.markdown("---")
 
